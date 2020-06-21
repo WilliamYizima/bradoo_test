3f5a9ce1d4a7bad98be95c3ef91f33f7e452a376
will <william.yizima@hotmail.com>
2020-06-20 21:18:34 -0300

    refactor

--- app/helper.py
+++ app/helper.py
@@ -1,9 +1,14 @@
 def cnpj_without_mask(cnpj):
-    cnpj = cnpj.replace('.','')
-    cnpj = cnpj.replace('-','')
-    cnpj = cnpj.replace('/','')
+    cnpj = cnpj.replace('.', '')
+    cnpj = cnpj.replace('-', '')
+    cnpj = cnpj.replace('/', '')
     return cnpj
+
 
 def cnpj_mask(cnpj):
     cnpj_clear = cnpj_without_mask(cnpj)
-    return "%s.%s.%s/%s-%s" % ( cnpj_clear[0:2], cnpj_clear[2:5], cnpj_clear[5:8], cnpj_clear[8:12], cnpj_clear[12:14] )
+    return "%s.%s.%s/%s-%s" % (cnpj_clear[0:2], 
+                               cnpj_clear[2:5], 
+                               cnpj_clear[5:8], 
+                               cnpj_clear[8:12], 
+                               cnpj_clear[12:14])
