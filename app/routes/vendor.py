767b4e6ad4c1899c355f184ac948db4f53bce461
will <william.yizima@hotmail.com>
2020-06-20 21:15:10 -0300

    refactor

--- app/routes/vendor.py
+++ app/routes/vendor.py
@@ -22,17 +22,17 @@ def add_vendor():
 @vendor.route('/<int:id_>', methods=['DELETE'])
 def delete_vendor(id_):
     try:
-            #TODO ddelete products before vendor
+# TODO ddelete products before vendor
         Vendor.query.filter(Vendor.id == id_).delete()
         current_app.db.session.commit()
-        return jsonify(f'Deletado {id_}'),200
+        return jsonify(f'Deletado {id_}'), 200
     except Exception as e:
-        return {'error':e},500
+        return {'error':e}, 500
 
 
 @vendor.route("/edit/<int:id_>", methods=['PUT'])
 def edit_vendor(id_):
-   
+
     try:
         vs = VendorSchema()
         query = Vendor.query.filter(Vendor.id == id_)
@@ -41,6 +41,5 @@ def edit_vendor(id_):
         return vs.jsonify(query.first())
 
     except Exception as e:
-        
+  
         return(str(e))
-
