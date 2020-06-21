1b6912d4f042122593cecc8c58e75fffed3bc4cb
will <william.yizima@hotmail.com>
2020-06-20 21:00:26 -0300

    flake8

--- app/routes/product.py
+++ app/routes/product.py
@@ -1,39 +1,39 @@
 #TODO IMPLANTAR
 
-# @app.route("/registerproduct",methods=['POST'])
-# def add_product():
-#     if request.method == 'POST':
-#         name=request.form.get('name')
-#         cnpj=request.form.get('code')
-#         city=request.form.get('price')
-#         try:
-#             product=Product(
-#                 name=name,
-#                 cnpj=code,
-#                 city=price
-#             )
-#             db.session.add(product)
-#             db.session.commit()
-#             return redirect(url_for('list'))
-#         except Exception as e:
-#             return(str(e))
-#     return render_template("list.html")
-#
-# 
-# @app.route('/get_cnj/<string:cnpj>')
-# def get_cnpj(cnpj):
-    # try:
-    #     vendor_cnpj = {'cnpj':'CNPJ not found'}
-    #     cnpj = cnpj_without_mask(cnpj)
-    #     vendor=Vendor.query.filter_by(cnpj=cnpj).first()
-    #     if(vendor):
-    #         vendor_cnpj =(vendor.serialize())
-    #     return vendor_cnpj 
-    # except Exception as e:
-    #     error=Errors(
-    #         end_point='/get_cnj/<cnpj>',
-    #         error=str(e)
-    #     )
-    #     db.session.add(error)
-    #     db.session.commit()
-    #     return 'I have A Problem'
+@app.route("/registerproduct",methods=['POST'])
+def add_product():
+    if request.method == 'POST':
+        name=request.form.get('name')
+        cnpj=request.form.get('code')
+        city=request.form.get('price')
+        try:
+            product=Product(
+                name=name,
+                cnpj=code,
+                city=price
+            )
+            db.session.add(product)
+            db.session.commit()
+            return redirect(url_for('list'))
+        except Exception as e:
+            return(str(e))
+    return render_template("list.html")
+
+
+@app.route('/get_cnj/<string:cnpj>')
+def get_cnpj(cnpj):
+    try:
+        vendor_cnpj = {'cnpj':'CNPJ not found'}
+        cnpj = cnpj_without_mask(cnpj)
+        vendor=Vendor.query.filter_by(cnpj=cnpj).first()
+        if(vendor):
+            vendor_cnpj =(vendor.serialize())
+        return vendor_cnpj 
+    except Exception as e:
+        error=Errors(
+            end_point='/get_cnj/<cnpj>',
+            error=str(e)
+        )
+        db.session.add(error)
+        db.session.commit()
+        return 'I have A Problem'
