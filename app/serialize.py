87ef5fbf9bd99eeaff0a5d90cb633db06e80ae2d
will <william.yizima@hotmail.com>
2020-06-20 21:28:23 -0300

    refactor

--- app/serialize.py
+++ app/serialize.py
@@ -1,20 +1,23 @@
 from flask_marshmallow import Marshmallow
-from models import Vendor,Product,Errors
+from models import Vendor, Product, Errors
 
 ma = Marshmallow()
 
+
 def configure(app):
     ma.init_app(app)
+
 
 class VendorSchema(ma.ModelSchema):
     class Meta:
         model = Vendor
 
+
 class ProductSchema(ma.Schema):
     class Meta:
         model = Product
 
+
 class ErrorsSchema(ma.Schema):
     class Meta:
         model = Errors
-
