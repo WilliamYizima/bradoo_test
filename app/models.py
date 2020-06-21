f41b9a966ea638bbb5b720a49bd9603e683c89ba
will <william.yizima@hotmail.com>
2020-06-20 21:24:15 -0300

    refactor

--- app/models.py
+++ app/models.py
@@ -34,14 +34,15 @@ class Vendor(db.Model):
             'city': self.city
         }
 
-class Product(db.Model):
-    
 
-    id = db.Column(db.Integer,primary_key = True)
-    code = db.Column(db.String,nullable = False, unique = True)
-    name = db.Column(db.String(),nullable = False, unique = False)
-    price = db.Column(db.Float(), nullable = False, unique = False)
-    vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'),nullable=False)
+class Product(db.Model):
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String, nullable=False, unique=True)
+    name = db.Column(db.String(),nullable=False, unique=False)
+    price = db.Column(db.Float(), nullable=False, unique=False)
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'), nullable=False)
 
     def __init__(self, code, name, vendor_id, price=0.00):
         self.name = name
